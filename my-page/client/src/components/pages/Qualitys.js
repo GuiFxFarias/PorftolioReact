4fb2589c0c55628800d92cfb3fad7bae1945c7cb
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2023-03-24 08:11:44 -0300

    fixed bugs

--- my-page/client/src/components/pages/Qualitys.js
+++ my-page/client/src/components/pages/Qualitys.js
@@ -12,53 +12,30 @@ function Qualitys() {
     "Clique na linguagem e aprenda um pouco sobre ela"
   );
 
-  const list = [
-    {
-      id: 1,
-      value: "Um",
-      src: css,
-    },
-    {
-      id: 2,
-      value: "Dois",
-      src: js,
-    },
-    {
-      id: 3,
-      value: "Tres",
-      src: ts,
-    },
-    {
-      id: 4,
-      value: "Quatro",
-      src: react,
-    },
-  ];
-
-  // function lang01() {
-  //   setLang(
-  //     "Com a utlização do CSS3 deixamos o site mais bonito, responsivo e atrativo para os clientes"
-  //   );
-  // }
-  // function lang02() {
-  //   setLang(
-  //     "Com a utilização do JavaScript realizamos suas interações e funcionalidade dentro do seu site, como enviar e-mail, clicar em alguma barra lateral, etc."
-  //   );
-  // }
-  // function lang03() {
-  //   setLang(
-  //     "Em ReactJs, se realiza toda a montagem do seu site pelo formato de componetização"
-  //   );
-  // }
-  // function lang04() {
-  //   setLang(
-  //   "Com o TypeScript é praticamente o mesmo uso do JavaScript, porém, Ts seria uma linguagem com mais tipagem e não tão dinâmica como é oJavaScript"
-  //   );
-  // }
+  function lang01() {
+    setLang(
+      "Com a utlização do CSS3 deixamos o site mais bonito, responsivo e atrativo para os clientes"
+    );
+  }
+  function lang02() {
+    setLang(
+      "Com a utilização do JavaScript realizamos suas interações e funcionalidade dentro do seu site, como enviar e-mail, clicar em alguma barra lateral, etc."
+    );
+  }
+  function lang03() {
+    setLang(
+      "Em ReactJs, se realiza toda a montagem do seu site pelo formato de componetização"
+    );
+  }
+  function lang04() {
+    setLang(
+      "Com o TypeScript é praticamente o mesmo uso do JavaScript, porém, Ts seria uma linguagem com mais tipagem e não tão dinâmica como é oJavaScript"
+    );
+  }
 
   return (
     <>
-      <div className="divPaiQuality" style={{backgroundColor: "red"}}>
+      <div className="divPaiQuality">
         <Element name="quality" className="elementQ">
           <div className="flexQ">
             <h1 className="h1">H</h1>
@@ -72,17 +49,26 @@ function Qualitys() {
             <h1 className="h1">d</h1>
             <h1 className="h1">e</h1>
             <h1 className="h1">s</h1>
-            <h1 style={{color: "white"}}>ESTA PARTE DO SITE ESTÁ EM MANUTENÇÃO</h1>
+            {/* <h1 style={{ color: "white" }}>
+              ESTA PARTE DO SITE ESTÁ EM MANUTENÇÃO
+            </h1> */}
           </div>
           <div className="flexQuality">
-            {list.map((item) => (
-              <div className="blocksLanguages">
-                <img src={item.src} key={item.id} alt="" />
-              </div>
-            ))}
+            <div className="blocksLanguages" onClick={lang01}>
+              <img src={css} alt="" />
+            </div>
+            <div className="blocksLanguages" onClick={lang02}>
+              <img src={js} alt="" />
+            </div>
+            <div className="blocksLanguages" onClick={lang04}>
+              <img src={react} alt="" />
+            </div>
+            <div className="blocksLanguages" onClick={lang03}>
+              <img src={ts} alt="" />
+            </div>
           </div>
           <div className="apresentationLanguage">
-            <p className="fadeIn">{lang}</p>
+            <p>{lang}</p>
           </div>
         </Element>
       </div>
